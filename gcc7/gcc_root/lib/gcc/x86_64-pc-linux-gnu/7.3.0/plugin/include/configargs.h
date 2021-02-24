80d0462692dfc7f0c9bd4a7560d6a74d6f035a7c
Reed Taylor <reedwtaylor@gmail.com>
2021-02-24 10:04:21 -0500

    initial commit

---
+++ gcc7/gcc_root/lib/gcc/x86_64-pc-linux-gnu/7.3.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/travis/gcc_src/configure --prefix /home/travis/gcc7_install/ --enable-languages=c --disable-bootstrap --enable-nls";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch", "x86-64" } };
